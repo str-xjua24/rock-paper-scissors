870cd5b793e068356fbe9fe08553f008c5f3572a
Joshua <juarez97@gmail.com>
2022-09-17 15:27:28 -0700

    Add game over after five wins; Game resets once player clicks on any button

--- rps_game.js
+++ rps_game.js
@@ -2,6 +2,11 @@ const choices = ["rock", "paper", "scissors"];
 let playerScore = 0, computerScore = 0;
 let result = "";
 let playerInput, userChoice, computerChoice;
+
+const player_wins = document.querySelector('#player-wins');
+const computer_wins = document.querySelector('#computer-wins');
+const input_btns = document.querySelectorAll('.input-btns');
+const resultText = document.querySelector('#result-text');
 const playerIcon = document.querySelector('#player-icon');
 const computerIcon = document.querySelector('#bot-icon');
 
@@ -16,19 +21,16 @@ function playRound(playerSelection, computerSelection) {
         playerIcon.src = 'images/rock.png';
         computerIcon.src = 'images/scissors.png';
         result = "Rock beats scissors. You win.";
-        console.log("You win");
         playerScore++;
       } else if (computerSelection == "paper") {
         playerIcon.src = 'images/rock.png';
         computerIcon.src = 'images/paper.png';
         result = "Paper beats rock. You lose.";
-        console.log("You lose");
         computerScore++;
       } else {
         playerIcon.src = 'images/rock.png';
         computerIcon.src = 'images/rock.png';
         result = "Tie";
-        console.log("Tie");
       }
       break;
     case "paper":
@@ -36,53 +38,39 @@ function playRound(playerSelection, computerSelection) {
         playerIcon.src = 'images/paper.png';
         computerIcon.src = 'images/rock.png';
         result = "Paper beats rock. You win.";
-        console.log("You win");
         playerScore++;
       } else if (computerSelection == "scissors") {
         playerIcon.src = 'images/paper.png';
         computerIcon.src = 'images/scissors.png';
         result = "Scissors beats paper. You lose.";
-        console.log("You lose");
         computerScore++;
       } else {
         playerIcon.src = 'images/paper.png';
         computerIcon.src = 'images/paper.png';
         result = "Tie";
-        console.log("Tie");
       }
       break;
     case "scissors":
       if (computerSelection == "paper") {
         playerIcon.src = 'images/scissors.png';
         computerIcon.src = 'images/paper.png';
-        console.log("You win");
+        result = "Scissors beats paper. You win."
         playerScore++;
       } else if (computerSelection == "rock") {
         playerIcon.src = 'images/scissors.png';
         computerIcon.src = 'images/rock.png';
         result = "Rock beats scissors. You lose.";
-        console.log("You lose");
         computerScore++;
       } else {
         playerIcon.src = 'images/scissors.png';
         computerIcon.src = 'images/scissors.png';
         result = "Tie";
-        console.log("Tie");
       }
       break;
   }
 }
 
 function game() {
-  const player_wins = document.querySelector('#player-wins');
-  const computer_wins = document.querySelector('#computer-wins');
-  const input_btns = document.querySelectorAll('.input-btns');
-  const resultText = document.querySelector('#result-text');
-
-  if (playerScore >= 5 || computerScore >= 5) {
-    button.disabled = true;
-  }
-
   input_btns.forEach((button) => {
 
     button.addEventListener('click', () => {
@@ -92,6 +80,17 @@ function game() {
       computer_wins.textContent = computerScore;
 
       resultText.textContent = result;
+      if (playerScore >= 5 || computerScore >= 5) {
+        if (playerScore >= 5) {
+          resultText.textContent = "Game Over. You Won!";
+        } else {
+          resultText.textContent = "Game Over. You Lost.";
+        }
+        playerScore = 0;
+        computerScore = 0;
+        playerIcon.src = 'images/player.png';
+        computerIcon.src = 'images/bot.png';
+      }
     });
   });
 }
